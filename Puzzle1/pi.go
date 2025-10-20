c73266910d93fd52e9530a065a6c50297d5a134d
misato <kawano.misato@gmail.com>
2025-10-20 23:55:46 +0900

    Solve Puzzle1: pi approximation

--- Puzzle1/pi.go
+++ Puzzle1/pi.go
@@ -1,1 +1,15 @@
-package Puzzle1
+package main
+
+import "fmt"
+
+func main() {
+	var π = 355 / 113.0 // 密率（みつりつ）
+	fmt.Println(π)
+
+	// var 🐭 = 355 / 113.0; // コンパイルエラー
+	// fmt.Println(🐭)
+
+	// a, b := 355, 113.0 // コンパイルエラー
+	// π = a / b
+	// fmt.Println(π)
+}
